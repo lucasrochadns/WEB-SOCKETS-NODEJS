66a7fd0d97bc2af3b532d617d647803af2dd3672
Lucas-Dev <lucas.rocha-dev@outlook.com>
2023-02-09 14:02:58 -0300

    Camada Services

--- public/document.js
+++ public/document.js
@@ -1,1 +1,28 @@
-const socket = io();
+import {
+    emitirTextoEditor,
+    selecionarDocumento,
+  } from "./socket-front-document.js";
+  
+  const parametros = new URLSearchParams(window.location.search);
+  const nomeDocumento = parametros.get("nome");
+  
+  const textoEditor = document.getElementById("editor-texto");
+  const tituloDocumento = document.getElementById("titulo-documento");
+  
+  tituloDocumento.textContent = nomeDocumento || "Documento sem título";
+  
+  selecionarDocumento(nomeDocumento);
+  
+  textoEditor.addEventListener("keyup", () => {
+    emitirTextoEditor({
+      texto: textoEditor.value,
+      nomeDocumento,
+    });
+  });
+  
+  function atualizaTextoEditor(texto) {
+    textoEditor.value = texto;
+  }
+  
+  export { atualizaTextoEditor };
+  
--- src/servidor.js
+++ src/servidor.js
@@ -5,22 +5,18 @@ import http from "http";
 import { Server } from "socket.io";
 
 const app = express();
-const porta = 3000;
+const porta = process.env.porta || 3000;
 
-const caminhoAltual = url.fileURLToPath(import.meta.url); // retorna o caminho atual
-const diretorioPublic = path.join(caminhoAltual, "../../", "public"); // acessa o caminho public
+const caminhoAtual = url.fileURLToPath(import.meta.url);
+const diretorioPublico = path.join(caminhoAtual, "../../", "public");
+app.use(express.static(diretorioPublico));
 
-app.use(express.static(diretorioPublic));
 const servidorHttp = http.createServer(app);
 
+servidorHttp.listen(porta, () =>
+  console.log(`Servidor escutando na porta ${porta}`)
+);
 
-
-servidorHttp.listen(porta, () => console.log(`Servidor rodando na porta ${porta}`));
 const io = new Server(servidorHttp);
 
-io.on("connection", () => {
-     console.log("Cliente se connectou");
-});
-
-
-// acessar pagina biblioteca socket.io 
+export default io;
